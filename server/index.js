c6230912ce3d8906bfe7d7c700cc802972706d8f
Your Name <you@example.com>
2026-03-10 02:30:17 +0600

    enable cross-origin cookie sending

--- server/index.js
+++ server/index.js
@@ -37,6 +37,7 @@ app.use(
         cookie: {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
+            sameSite: "none",
             maxAge: 24 * 60 * 60 * 1000
         }
     })
